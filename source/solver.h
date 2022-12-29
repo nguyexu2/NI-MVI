af7ba0c47d083707091fd3ca11d9c42a5a953956
nguyexu2 <nguyexu2@fit.cvut.cz>
2022-12-29 02:50:43 +0100

    population init

--- source/solver.h
+++ source/solver.h
@@ -9,14 +9,40 @@ assignments defaultAssigment(const int varCount)
 	return ret;
 }
 
+assignments randomAssigment(const int varCount)
+{
+	assignments ret(varCount + 1); // reserve 0
+	FOR(i, 1, ret.size()) ret[i] = i * ( getrandInt(0, 1) ? 1 : -1 );
+	return ret;
+}
+
+bool existsSolution(const formula & instance, const vector<assignments> & population)
+{
+	for(const auto & config : population) if(instance.isSat(config)) return true;
+	return false;
+}
+
 assignments solve(const formula & instance)
 {
-	auto asgn = defaultAssigment(instance.variables);
-
-	while(!instance.isSat(asgn))
+	const int populationSize = 100;
+	vector<assignments> population;
+	FOR(i, 0, populationSize) population.push_back(randomAssigment(instance.variables));
+	//creates sorted population by number of solved clauses
+	sort(population.begin(), population.end(), [&](const auto &a, const auto &b)
+		 { return instance.solvedClauses(a) > instance.solvedClauses(b); });
+		 
+	const int maxIterations = 5000;
+	FOR(it, 0, maxIterations)
 	{
-		auto prev = asgn;	
+		if(existsSolution(instance, population))
+			break;
+
+		// selection
+
+		// crossing
+
+		// mutation
 	}
 
-	return asgn;
+	return population.front();
 }
