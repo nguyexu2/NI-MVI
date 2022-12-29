3d4bc80e7a01002a04ceb3f6667bb7bdba55d885
nguyexu2 <nguyexu2@fit.cvut.cz>
2022-12-29 02:50:06 +0100

    rng

--- source/include.cpp
+++ source/include.cpp
@@ -9,3 +9,16 @@ typedef int ll;
 #define deb(x) std::cerr<<#x<<" "<<x<<std::endl;
 template<typename T> ostream& operator<<(ostream& out, const vector<T> & v){for(const auto & x : v){out << x << ' ';} return out;}
 template<typename T> istream& operator>>(istream& in, vector<T>&v){for(auto&x:v){in>>x;}return in;}
+
+std::mt19937 rng;
+
+void initRNG()
+{
+	rng = std::mt19937(1000);
+}
+
+int getrandInt(int lb, int ub)
+{
+	std::uniform_int_distribution<std::mt19937::result_type> generator(lb , ub);
+	return generator(rng);
+}
