50b41b081ecabbc5ac1352ffd42fd44dd3fb1f0f
nguyexu2 <nguyexu2@fit.cvut.cz>
2022-12-29 02:50:58 +0100

    selection

--- source/genetics.h
+++ source/genetics.h
@@ -1,3 +1,51 @@
 #pragma once
 #include "include.cpp"
 #include "structures.h"
+
+//------------------------------
+//selection of population
+vector<assignments> roulette(const formula &instance, const vector<assignments> &population,
+							 double elitismRate)
+{
+	vector<assignments> ret;
+
+	return ret;
+}
+
+vector<assignments> tournament(const formula &instance, const vector<assignments> &population,
+							   double probability, double elitismRate)
+{
+
+}
+
+//------------------------------
+// operators
+
+assignments crossover(const assignments & a, const assignments & b, int splits)
+{
+	assignments ret = a;
+	set<int> splitPos;
+	FOR(i, 0, splits) splitPos.insert(getrandInt(0, a.size()));
+
+	vector<int> vSplitPos(splitPos.begin(), splitPos.end());
+	FOR(i, 0, vSplitPos.size() - 1)
+	{
+		auto s = vSplitPos[i], e = vSplitPos[i+1];
+		FOR(j, s, e) ret[j] = i % 2? a[j] : b[j];
+	}
+	
+	return ret;
+}
+
+assignments mutation(const assignments & config, double changeRate)
+{
+
+}
+
+//------------------------------
+//fitness
+
+int getFitness(const formula & instance, const assignments & config)
+{
+	return instance.solvedClauses(config);
+}
