af19e5ae53f3ff1aecd52afa756720d7c58e087d
nguyexu2 <nguyexu2@fit.cvut.cz>
2022-12-29 19:55:28 +0100

    crossover filling and mutation

--- source/genetics.h
+++ source/genetics.h
@@ -21,7 +21,7 @@ vector<assignments> tournament(const formula &instance, const vector<assignments
 //------------------------------
 // operators
 
-void crossover(assignments & a, assignments & b, int splits = 1)
+pair<assignments, assignments> crossover(assignments a, assignments b, int splits = 1)
 {
 	set<int> splitPos;
 	FOR(i, 0, splits) splitPos.insert(getrandInt(0, a.size()));
@@ -32,11 +32,36 @@ void crossover(assignments & a, assignments & b, int splits = 1)
 		FOR(idx, vSplitPos[i], vSplitPos[i+1])
 			swap(a[idx], b[idx]);
 	}
+	return {a, b};
 }
+
+void fillThePopulation(const formula &instance, vector<assignments> &population, int finalPopulationSize)
+{
+	int oldCnt = population.size(), cnt = finalPopulationSize - oldCnt;
+	FOR(i, 0, cnt)
+	{
+		int first, second;
+		do {first = getrandInt(0, oldCnt), second = getrandInt(0, oldCnt);} while(first == second);
+		auto [ch1, ch2] = crossover(population[first], population[second]);
+
+		//take the better of 2 children
+		population.push_back(instance.solvedClauses(ch1) > instance.solvedClauses(ch2) ? ch1 : ch2);
+
+	}
+}
+
+//------------------------------
 
 assignments mutation(const assignments & config, double changeRate)
 {
+	assignments ret = config;
+	int cnt = 1; //todo: generate
 
+	set<int> flipPos;
+	FOR(i, 0, cnt) flipPos.insert(getrandInt(1, config.size()));
+
+	for(auto pos : flipPos) ret[pos] = -ret[pos];
+	return ret;
 }
 
 //------------------------------
